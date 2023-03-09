9e99248f2c4660700beb1d575d1523c2fa3e3298
Pongsaky <pongsakon.kawe@gmail.com>
2023-03-09 15:46:54 +0700

    change recommend fucntion

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def read_root():
     return "Hello Platea.co"
 
-@app.post("/recommendV1/{tripLevel}")
+@app.post("/recommend/")
 def recommend(art_level:int, history_level:int, nature_level:int, shopping_level:int, k:int=10):
     model = TFID_MODEL()
     k_recommend = model.recommend([art_level, history_level, nature_level, shopping_level], k)
