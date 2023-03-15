3652cca90327223ee1e3bddad1ef75e8f92000e0
Pongsaky <pongsakon.kawe@gmail.com>
2023-03-15 17:16:23 +0700

    Add CORS to allow *

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from model.model import TFID_MODEL
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
