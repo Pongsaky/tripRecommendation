ac22dc7b86f470934e03f8b16bd7683e1c78a5e8
Pongsaky <pongsakon.kawe@gmail.com>
2023-03-16 17:14:10 +0700

    add planning feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from model.model import TFID_MODEL
+from model.model import Recommendation_MODEL
+import datetime
+import time
 
 app = FastAPI()
 
@@ -16,8 +18,14 @@ app.add_middleware(
 def read_root():
     return "Hello Platea.co"
 
-@app.post("/recommend/")
+@app.get("/recommend/")
 def recommend(art_level:int, history_level:int, nature_level:int, shopping_level:int, k:int=10):
-    model = TFID_MODEL()
-    k_recommend = model.recommend([art_level, history_level, nature_level, shopping_level], k)
+    model = Recommendation_MODEL([art_level, history_level, nature_level, shopping_level])
+    k_recommend = model.recommend(k)
     return k_recommend
+
+@app.get("/get_planning/")
+def planning(art_level:int, history_level:int, nature_level:int, shopping_level:int, milli_start_time:int, placePerDay:int, day:int, timePerDay:int):
+    model = Recommendation_MODEL([art_level, history_level, nature_level, shopping_level])
+    planning = model.planning(milli_start_time=milli_start_time, placePerDay=placePerDay, day=day, timePerDay=timePerDay)
+    return planning
--- model/model.py
+++ model/model.py
@@ -1,17 +1,30 @@
 import pandas as pd
 import numpy as np
 import random
+import datetime
+import time
+import pickle
+from math import radians
+
+from gensim.models import KeyedVectors
 
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import haversine_distances
 
-class TFID_MODEL:
-    def __init__(self):
+class Recommendation_MODEL:
+    def __init__(self, trip_level:list):
+        self.trip_level = trip_level
+        
         self.detail = pd.read_csv("./model/detailTFIDF.csv")
         self.webDetail = self.get_webDetail()
 
-        self.term_vector = self.get_termVector()
+        self.term_vector_tfidf = self.get_term_vector_tfidf()
+        self.term_vector_word2vec = self.get_term_vector_word2vec()
         self.type_list = self.get_type_list()
         self.type_vector = self.get_type_vector()
+
+        self.visited = np.zeros((len(self.detail)+1,))
+        self.visited[62] = 1
 
     def get_webDetail(self):
         webDetail = pd.read_csv('./data/file/finalPlace.csv')
@@ -28,7 +41,7 @@ class TFID_MODEL:
         return webDetail
 
     # matrix of tfidf score
-    def get_termVector(self):
+    def get_term_vector_tfidf(self):
         term_vector = np.array([np.fromstring(self.detail['tfidf'].to_numpy()[0][1:-1], sep=',')])
 
         for i in range(len(self.detail) - 1):
@@ -36,6 +49,12 @@ class TFID_MODEL:
             term_vector = np.append(term_vector, term_vector_i, axis=0)
         
         return term_vector
+    
+    def get_term_vector_word2vec(self):
+        with open('./model/term_word2vec.pkl', 'rb') as file:
+            term_vctor_word2vec = pickle.load(file)
+            file.close()
+        return term_vctor_word2vec
 
     def get_type_list(self):
         # Type from Place API (Too much type)
@@ -107,18 +126,32 @@ class TFID_MODEL:
         return all_random_index
 
 
-    def get_term_score(self, all_random_index):
+    def get_term_score(self, all_random_index, model):
         # Term vector
         # Select vector from upper index
-        trip_term_vector = np.zeros(self.term_vector[0].shape)
+        if model=='tfidf':
+            # Term vector
+            # Select vector from upper index
+            trip_term_vector = np.zeros(self.term_vector_tfidf[0].shape)
 
-        for idx_vector in all_random_index:
-            trip_term_vector += self.term_vector[idx_vector]
+            for idx_vector in all_random_index:
+                trip_term_vector += self.term_vector_tfidf[idx_vector]
 
-        trip_term_vector = trip_term_vector / len(all_random_index)
-        trip_term_score = cosine_similarity(np.append(self.term_vector, trip_term_vector.reshape(1,-1), axis=0))
+            trip_term_vector = trip_term_vector / len(all_random_index)
+            trip_term_score = cosine_similarity(np.append(self.term_vector_tfidf, trip_term_vector.reshape(1,-1), axis=0))
+            return trip_term_score
+        if model=='word2vec':
+            # Term vector
+            # Select vector from upper index
+            trip_term_vector = np.zeros(self.term_vector_word2vec["เซ็นทรัลเวิลด์"].shape)
 
-        return trip_term_score
+            for idx_vector in all_random_index:
+                trip_term_vector += self.term_vector_word2vec[self.detail.iloc[idx_vector, 1]]
+
+            trip_term_vector = trip_term_vector / len(all_random_index)
+            trip_term_score = cosine_similarity(np.append(np.array(list(self.term_vector_word2vec.values())), trip_term_vector.reshape(1,-1), axis=0))
+            return trip_term_score
+
 
     def get_type_score(self, all_random_index):
         # Type vector
@@ -132,29 +165,89 @@ class TFID_MODEL:
 
         return trip_type_score
     
-    def recommend(self, trip_level:list, k:int):
-        random_index = self.get_random_index(trip_level)
-        trip_term_score = self.get_term_score(random_index)
+    # Distance (lat,long)
+    def get_distance_score(self, place_index):
+        start_idx = place_index
+
+        distance = np.zeros((62,))
+        geometry_list = self.detail['geometry'].map(lambda x  : eval(x)).to_list()
+        
+        current_point = eval(self.detail.iloc[start_idx]['geometry'])
+        self.visited[start_idx] = 1
+
+        current_radian = [radians(_) for _ in current_point]
+
+        for idx, dest_point in enumerate(geometry_list):
+            dest_radian = [radians(_) for _ in dest_point]
+            distance[idx] = haversine_distances([current_radian, dest_radian])[0][1] * 6371 # 6371 is radius of the Earth
+        
+        return np.append(1/distance, np.Inf)
+    
+    def recommend(self, k:int, model="word2vec"):
+        random_index = self.get_random_index(self.trip_level)
+        trip_term_score = self.get_term_score(random_index, model=model)
         trip_type_score = self.get_type_score(random_index)
 
         # Rating score
         rating_score = self.webDetail['rating'].to_numpy()
         rating_score = np.append(rating_score, 5)
+        # Distance score
+        trip_distance_score = np.zeros((len(self.detail),))
+        trip_distance_score = np.append(trip_distance_score, np.Inf)
+        
+        k_recommed = []
+        for i in range(k):
+            # tfidf term_score is important score because the different of value are far
+            relevance_score = trip_term_score[-1]*2 + trip_type_score[-1]*1 + rating_score*0.05 + trip_distance_score * 1
+            # Recommendation
+            place_recommended = sorted(list(enumerate(relevance_score.reshape(-1))), reverse=True, key=lambda x:x[1])
+            for idx, similar in place_recommended:
+                # print(idx)
+                if not self.visited[idx]:
+                    k_recommed.append({
+                        "idx" : idx,
+                        "name" : self.detail.iloc[idx]['name'],
+                        "address" : self.detail.iloc[idx]['address'],
+                        "geometry" : { "lat" : self.detail.iloc[idx]['geometry'].split(',')[0][1:],
+                                    "lon": self.detail.iloc[idx]['geometry'].split(',')[-1][:-1]
+                                    },
+                        "phone_number" : self.detail.iloc[idx]['phone_number'],
+                        })
+                    self.visited[idx]=1
+                    break
 
-        relevance_score = trip_term_score[-1]*0.5 + trip_type_score[-1]*0.3 + rating_score*0.2
-
-        # Recommendation
-        k_recommed = {}
-        distances = sorted(list(enumerate(relevance_score.reshape(-1))), reverse=True, key=lambda x:x[1])
-        for idx, i in enumerate(distances[1:k+1]): # Top 10
-            k_recommed[idx] = {
-                "ID" : self.detail.iloc[i[0]]['placeID'],
-                "name" : self.detail.iloc[i[0]]['name'],
-                "address" : self.detail.iloc[i[0]]['address'],
-                "geometry" : { "lat" : self.detail.iloc[i[0]]['geometry'].split(',')[0][1:],
-                               "lon": self.detail.iloc[i[0]]['geometry'].split(',')[-1][:-1]
-                            },
-                "phone_number" : self.detail.iloc[i[0]]['phone_number'],
-                }
-            # print(self.detail.iloc[i[0]]['name'])
         return k_recommed
+    
+    def planning(self, milli_start_time, placePerDay:int, day:int, timePerDay:int):
+        if placePerDay*day > len(self.detail):
+            return {"Error" : "input error"}
+        # input time_start is milliseonds form
+        k_recommend = self.recommend(len(self.detail))
+        HourPerPlace = int(timePerDay / placePerDay)
+        MinPerPlace = ((timePerDay/placePerDay)*60) - (HourPerPlace*60)
+
+        planning = {}
+        idx = 0
+
+        for d in range(day):
+            placeIn = []
+            time_start = datetime.datetime.fromtimestamp(milli_start_time/1000) + datetime.timedelta(days=d)
+            for j in range(placePerDay):
+                time_end = time_start + datetime.timedelta(hours=HourPerPlace, minutes=MinPerPlace)
+                milli_time_start = int(time.mktime(time_start.timetuple())) * 1000
+                milli_time_end = int(time.mktime(time_end.timetuple())) * 1000
+
+                placeIn.append({
+                    'name': k_recommend[idx]["name"],
+                    'geometry': k_recommend[idx]['geometry'],
+                    'address' : k_recommend[idx]['address'],
+                    'phone_number': k_recommend[idx]['phone_number'],
+                    'time': {
+                            "time_start": milli_time_start,
+                            "time_end" :  milli_time_end
+                            }
+                })
+                time_start = time_end
+                idx+=1
+            planning[str(d+1)] = placeIn
+        return planning
